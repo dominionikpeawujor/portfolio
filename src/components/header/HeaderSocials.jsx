5c0584c9ab372491d2fb2c99a1f24835d2db0e4b
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-04 15:59:37 +0100

    edit github link

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -10,7 +10,7 @@ const HeaderSocials = () => {
         href="https://www.linkedin.com/in/dominion-ikpeawujor"
         target="_blank" rel="noreferrer"
       ><BsLinkedin/></a>
-      <a href="htps://www.github.com/dominion321" target="_blank" rel="noreferrer"><FaGithub/></a>
+      <a href="htps://www.github.com/dominionikpeawujor" target="_blank" rel="noreferrer"><FaGithub/></a>
       <a href="htps://www.twitter.com/dominionci" target="_blank" rel="noreferrer"><FaTwitter/></a>
     </div>
   );
